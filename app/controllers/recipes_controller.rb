450ccc94aa7e1661588c9639c7282dc7d2072d9a
MatsunagaNaoya <60386488+MatsunagaNaoya@users.noreply.github.com>
2022-10-28 19:22:49 +0900

    controller記述

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,45 @@
 class RecipesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
+    @recipes = Recipe.includes(:user).order('created_at DESC')
   end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.create(recipe_params)
+    if @recipe.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+#  def show
+#  end
+
+#  def edit
+#  end
+
+#  def update
+#  end
+
+#  def destroy
+#  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :info, :prefecture_id,
+                                 :price).merge(user_id: current_user.id)
+  end
+
+#  def set_item
+#    @recipe = Recipe.find(params[:id])
+#  end
+
 
 end
