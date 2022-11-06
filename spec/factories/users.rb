f9328b0791e10939c8452238b8970169ee21de30
MatsunagaNaoya <60386488+MatsunagaNaoya@users.noreply.github.com>
2022-11-06 10:40:28 +0900

    テストコード記入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { 'a1' + Faker::Internet.password(min_length: 4) }
+    password_confirmation { password }
+    prefecture_id         { Faker::Number.between(from: 2, to: 48) }
   end
 end
