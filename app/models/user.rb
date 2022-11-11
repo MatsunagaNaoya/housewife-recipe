287708d21cebc9a9c9c4f644d99fccc33637d18e
MatsunagaNaoya <60386488+MatsunagaNaoya@users.noreply.github.com>
2022-11-11 13:21:59 +0900

    モデル設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
+  def liked_by?(recipe_id)
+    likes.where(recipe_id: recipe_id).exists?
+  end
+
   with_options presence: true do
     validates :nickname
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
